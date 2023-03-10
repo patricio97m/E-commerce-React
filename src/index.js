89fab549cbe95fc46438aab0b7d57392f0420d68
patricio97m <patricio.97m@gmail.com>
2023-03-09 21:04:42 -0300

    implementación de firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEpRKk9hQSeRo1KWiPAWwUH9KpqFHElRA",
+  authDomain: "e-commerce-react-pm.firebaseapp.com",
+  projectId: "e-commerce-react-pm",
+  storageBucket: "e-commerce-react-pm.appspot.com",
+  messagingSenderId: "684746592809",
+  appId: "1:684746592809:web:3fdd7ce1d30386468f4238"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
